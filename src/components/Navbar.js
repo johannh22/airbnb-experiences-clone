564b011b5f7939c143f4951e1634c1577e4bca01
Johann <62679700+johannh22@users.noreply.github.com>
2022-04-06 16:05:31 -0300

    Navbar included

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../images/logo.svg";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={logo} alt="logo" />
+    </nav>
+  )
+}
